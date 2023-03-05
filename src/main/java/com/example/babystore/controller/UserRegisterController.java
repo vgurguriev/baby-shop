b327e066f9c6cac469f371207bbea2023212cdce
vgurguriev <v.gurguriev@gmail.com>
2023-03-05 15:06:42 +0200

    Initial commit.

--- src/main/java/com/example/babystore/controller/UserRegisterController.java
+++ src/main/java/com/example/babystore/controller/UserRegisterController.java
@@ -3,15 +3,16 @@ package com.example.babystore.controller;
 import com.example.babystore.exception.DuplicationException;
 import com.example.babystore.model.dto.UserRegistrationDto;
 import com.example.babystore.service.UserService;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
@@ -34,31 +35,24 @@ public class UserRegisterController {
         return new UserRegistrationDto();
     }
 
-//    @PostMapping("/register")
-//    public String register(@Valid UserRegistrationDto userRegistrationDto,
-//                           BindingResult bindingResult,
-//                           RedirectAttributes redirectAttributes) throws DuplicationException {
-//
-//        if (bindingResult.hasErrors()) {
-//            redirectAttributes.addFlashAttribute("userRegistrationDto", userRegistrationDto);
-//            redirectAttributes.addFlashAttribute(
-//                    "org.springframework.validation.BindingResult.userRegistrationDto", bindingResult);
-//            return "redirect:/users/register";
-//        }
-//
-//        this.userService.registerAndLogin(userRegistrationDto);
-//        return "redirect:/";
-//    }
-
     @PostMapping("/register")
-    public String register(UserRegistrationDto userRegistrationDto, Model model) {
-        try {
-            userService.registerAndLogin(userRegistrationDto);
-        } catch (DuplicationException e) {
-            model.addAttribute("error", e);
-            return "redirect:/400";
-        }
+    public String register(@Valid UserRegistrationDto userRegistrationDto,
+                           BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes,
+                           Model model) throws DuplicationException {
 
-        return "redirect:/";
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("userRegistrationDto", userRegistrationDto);
+            redirectAttributes.addFlashAttribute(
+                    "org.springframework.validation.BindingResult.userRegistrationDto", bindingResult);
+            return "redirect:/users/register";
+        }
+        try {
+            this.userService.registerAndLogin(userRegistrationDto);
+            return "redirect:/";
+        } catch (DuplicationException e) {
+            model.addAttribute("ex", "Invalid username");
+        return "cart";
+        }
     }
 }
